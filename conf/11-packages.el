f4c9688b4b7c90e6879ce0bec6934eda1901433d
circularuins <natsuhiko.wake@gmail.com>
2012-04-11 01:47:40 +0900

    bug-fix

--- conf/11-packages.el
+++ conf/11-packages.el
@@ -49,7 +49,7 @@
 
 ;;; ブラウザでgoogle検索
 (load "google")
-()
+
 ;;; 補完候補の表示
 ;;(add-to-list 'load-path "~/src/auto-complete/auto-complete-1.3.1")
 ;; (require 'auto-complete)
@@ -218,7 +218,7 @@
 (require 'sl)
 
 ;;; Nyanmode (*^o^*)
-;; .emacs.dで "git submodule add https://github.com/TeMPOraL/nyan-mode.git ./lisp/nyan-mode" を実効
+;; .emacs.dで "git submodule add https://github.com/TeMPOraL/nyan-mode.git ./public_repos/nyan-mode" を実行
 (require 'nyan-mode)
 (nyan-mode t)
 (nyan-start-animation)
@@ -369,6 +369,25 @@
 ;;; e2wm 設定(終了する場合は「C-c ; Q」)
 (require 'e2wm)
 (global-set-key (kbd "M-+") 'e2wm:start-management)
+(e2wm:add-keymap 
+ e2wm:pst-minor-mode-keymap
+ '(("<M-left>" . e2wm:dp-code ) ; codeへ変更
+   ("<M-right>"  . e2wm:dp-two) ; twoへ変更
+   ("<M-up>"    . e2wm:dp-doc)  ; docへ変更
+   ("<M-down>"  . e2wm:dp-dashboard) ; dashboardへ変更
+   ("C-."       . e2wm:pst-history-forward-command) ; 履歴進む
+   ("C-,"       . e2wm:pst-history-back-command) ; 履歴戻る
+   ("C-M-s"     . e2wm:my-toggle-sub) ; subの表示をトグルする
+   ("prefix L"  . ielm) ; ielm を起動する（subで起動する）
+   ("M-m"       . e2wm:pst-window-select-main-command) ; メインウインドウを選択する
+   ) e2wm:prefix-key)
+(e2wm:add-keymap 
+ e2wm:dp-doc-minor-mode-map 
+ '(("prefix I" . info)) ; infoを起動する
+ e2wm:prefix-key)
+(defun e2wm:my-toggle-sub () ; Subをトグルする関数
+  (interactive)
+  (e2wm:pst-window-toggle 'sub t 'main))
 
 ;;;;この下に設定を追加しない！！
 ;;; 試行錯誤用ファイル
