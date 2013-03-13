80852a0fc3fff6261de9df11dfc579435253c75d
circularuins <ubuntu11@circularuins.(none)>
2013-03-13 13:18:19 +0900

    slimeでpare-edit

--- conf/23-programming.el
+++ conf/23-programming.el
@@ -117,6 +117,7 @@
 (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
 (add-hook 'lisp-mode-hook 'enable-paredit-mode)
 (add-hook 'ielm-mode-hook 'enable-paredit-mode)
+(add-hook 'slime-repl-mode)
 
 ;;; eldocの設定
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
