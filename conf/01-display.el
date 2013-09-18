8b774bc33be86ca05c7d84cf17262d800886f445
circularuins <ubuntu11@circularuins.(none)>
2013-09-18 15:58:22 +0900

    00-basic.elを廃止し、01-edit.elと02-display.elの順番を入れ替えた

--- conf/01-display.el
+++ conf/01-display.el
@@ -1,3 +1,47 @@
+;;;;;;;;;;;;;;;;
+;;; 基本設定 ;;;
+;;;;;;;;;;;;;;;;
+
+;;; 起動画面の非表示
+(setq inhibit-startup-message t)
+
+;;; 警告音なし
+(setq ring-bell-function 'ignore)
+
+;;; キーワードのカラー表示を有効化
+(global-font-lock-mode t)
+
+;;; 選択範囲（リージョン）をハイライト
+(transient-mark-mode 1)
+
+;;; 対応する括弧をハイライト
+(setq show-paren-delay 0)
+(show-paren-mode 1)
+(setq show-paren-style 'mixed)
+
+;;; 1行ずつスクロール
+(setq scroll-step 1)
+
+;;; タブの代わりにスペースを使う
+(setq-default tab-width 4 indent-tabs-mode nil)
+
+;;; "yes or no"の代わりに"y or n"を使用
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;;; evalした結果を全部表示
+(setq eval-expression-print-length nil)
+
+;;; 補完時に大文字小文字を区別しない
+(setq completion-ignore-case t)
+(setq read-file-name-completion-ignore-case t)
+
+;;; キーストロークをエコーエリアに早く表示する
+(setq echo-keystrokes 0.1)
+
+
+
+
+
 ;;;;;;;;;;;;;;;;;;;;
 ;;; フォント設定 ;;;
 ;;;;;;;;;;;;;;;;;;;;
