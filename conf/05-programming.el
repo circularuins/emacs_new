7048ea62741821223f9712e2c665c72c9dab0a42
circularuins <ubuntu11@circularuins.(none)>
2013-10-07 18:35:41 +0900

    flymakeのハイライトをアンダーラインに変更（CUI）

--- conf/05-programming.el
+++ conf/05-programming.el
@@ -79,6 +79,21 @@
 
 
 
+;;;;;;;;;;;;;;;
+;;; flymake ;;;
+;;;;;;;;;;;;;;;
+
+;; flymakeのハイライトをunderlineに変更
+;; ハイライトだとCUIで文字が見えないので、、
+(unless window-system
+  (custom-set-faces
+ '(flymake-errline ((((class color)) (:underline "red"))))
+ '(flymake-warnline ((((class color)) (:underline "yellow"))))))
+
+
+
+
+
 ;;;;;;;;;;;;
 ;;; Perl ;;;
 ;;;;;;;;;;;;
